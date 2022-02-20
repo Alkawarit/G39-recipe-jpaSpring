3a027c1a6a0dcb7dd3f0d6ab33047d77dda52906
Alkawarit <a.a.alkawarit@gmail.com>
2022-02-20 22:41:29 +0100

    Adding the app exception

---
+++ src/main/java/se/lexicon/aladdin/g39recipejpaspring/exception/AppResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package se.lexicon.aladdin.g39recipejpaspring.exception;
+
+public class AppResourceNotFoundException extends RuntimeException{
+    public AppResourceNotFoundException(String message){
+        super(message);
+    }
+}
